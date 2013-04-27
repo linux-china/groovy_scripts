a60d23d0fe908e5474f4fa562903242ea2fb09ba
Jacky Chan <libing.chen@gmail.com>
2013-04-27 14:41:11 +0800

    add @Grab

--- hello.groovy
+++ hello.groovy
@@ -1,8 +1,15 @@
 #!/usr/bin/env groovy
 
+@Grab(group = 'commons-lang', module = 'commons-lang', version = '2.6')
+import org.apache.commons.lang.StringUtils;
+
 /**
  * demo script
  *
  * @author linux_china
  */
 println("Hello World!");
+
+if (StringUtils.isNumeric("123")) {
+  println("This is Number");
+}
